0bb45c80bc97c41470f26cc834c81b6fab1579e2
Lukas Thiersch <lukas.thiersch@lansco.de>
2021-04-22 14:47:15 +0200

    Submission and resub

--- sheet-1/submission/ex2_Niklas-Markert_Lukas-Thiersch.py
+++ sheet-1/submission/ex2_Niklas-Markert_Lukas-Thiersch.py
@@ -9,14 +9,8 @@
 
 # a) What does inf mean? When does it occur in Python?
 
-# The abbreviation inf stands for infinity. While it is technically not possible
-# to represent infinity because integers or data stored in a length of bits
-# is limited by the length of bits and is therefore not infinite, you can
-# represent infinity in python with float('inf') or float('-inf').
-# Pythons maths module contains a value for it too: math.inf
-
-# It represents the largest positive or negative number representable by an int
-# => 1e1000 or -1e1000
+# The abbreviation inf stands for infinity. It occurs when an integer gets
+# larger than the largest integer that can be represented by the type.
 
 # ------------------------------------------------------------------------------
 
@@ -48,10 +42,10 @@
 # The following script checks 'near equality'
 
 def are_floats_nearly_equal(a, b):
-    # We check if the percentage of the deviation between a, b
+    # We check if the deviation between a, b
     # is less than the tolerated deviation
     tolerated_deviation = 0.001
-    return abs((a - b) / b) < tolerated_deviation
+    return abs(a - b) < tolerated_deviation
 
 
 x = float(input('Please provide the first floating point number: '))
